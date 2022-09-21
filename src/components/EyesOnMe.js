38360503ae18617b7d00856614b309922ce73448
Evans Ochieng <evansochieng121@gmail.com>
2022-09-21 19:56:42 +0300

    Completed working through react's event handlers

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+// Define callback functions
+function handleFocus(){
+    console.log('Good!');
+}
+
+function handleBlur(){
+    console.log('Hey! Eyes on me!');
+}
+
+const EyesOnMe = () => {
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
+
+// Console log on button click
+function handleChange(){
+    console.log('Entering password...')
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange}/>
+        </div>
     )
 }
 
